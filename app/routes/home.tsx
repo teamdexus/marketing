504a264cac756d60999c09a6d7598da87155e87f
Derrick Yoo <derrick@teamdexus.com>
2025-01-28 17:45:32 -0800

    Init project with GitHub workflows

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,13 +1,16 @@
-import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
+import type { Route } from './+types/home';
+import { Welcome } from '../welcome/welcome';
 
 export function meta({}: Route.MetaArgs) {
-  return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
-  ];
+	return [
+		{ title: 'Team Dexus' },
+		{
+			name: 'description',
+			content: 'AI Driven Platform & Consulting Services',
+		},
+	];
 }
 
 export default function Home() {
-  return <Welcome />;
+	return <Welcome />;
 }
